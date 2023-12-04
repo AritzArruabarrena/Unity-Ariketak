6659e4d1908c4f070e1c1902a95b697abae5c14f
Aritz <arruabarrena.aritz@uni.eus>
2023-12-04 14:24:47 +0100

    asdfasdfaaa

--- Ariketa1/Assets/Mugimendua.cs
+++ Ariketa1/Assets/Mugimendua.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,26 +6,70 @@ using UnityEngine;
 
 public class Mugimendua : MonoBehaviour
 {
-    public float velocidad = 5f;
-    public string dir = "DERECHA";
+    public float speed = 5f;
+    public string direction;
+    public Vector3 movement;
+    public Boolean giratzendago = false;
+    public float giratu;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        // Define the initial position
+        transform.position = Vector3.zero;
+        direction = "Forward";
+        movement = Vector3.forward;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (transform.position.x > 10f)
+        var angles = transform.rotation.eulerAngles;
+        if (!giratzendago) { 
+        // Move the object
+        transform.position += movement * speed * Time.deltaTime;
+            // Define the movement depending on each direction
+        if (direction.Equals("Forward") && transform.position.z >= 10)
         {
-            transform.position += Vector3.right;
-        } else if (transform.position.z > 10f)
+            //transform.rotation = Quaternion.Euler(0, 90, 0);
+            direction = "Right";
+            movement = Vector3.right;
+            giratzendago=true;
+        }
+        else if (direction.Equals("Right") && transform.position.x >= 10)
         {
-            transform.position += Vector3.forward;
-        } else if (transform.position.x <= 10f)
+            //transform.rotation = Quaternion.Euler(0, 180, 0);
+            direction = "Back";
+            movement = Vector3.back;
+            giratzendago=true;
+        }
+        else if (direction.Equals("Back") && transform.position.z <= 0)
         {
-            transform.position += Vector3.left;
+            //transform.rotation = Quaternion.Euler(0, 270, 0);
+            direction = "Left";
+            movement = Vector3.left;
+            giratzendago=true;
+        }
+        else if (direction.Equals("Left") && transform.position.x <= 0)
+        {
+            //transform.rotation = Quaternion.Euler(0, 0, 0);
+            direction = "Forward";
+            movement = Vector3.forward;
+            giratzendago=true;
+        }
+        } else {
+            if (angles.y <= 90)
+            {
+                angles.y += speed * Time.deltaTime * 10;
+                transform.rotation = Quaternion.Euler(angles);
+            }
+            else
+            {
+                giratzendago = false;
+            }
         }
     }
 }
+
+
